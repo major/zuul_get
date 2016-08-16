e65fc6bb766d28e34a995f348db727db217e89fc
Major Hayden <major@mhtx.net>
2016-08-16 12:01:35 -0500

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ required_packages = [
 
 setup(
     name='zuul_get',
-    version='0.0.1',
+    version='0.0.2',
     author='Major Hayden',
     author_email='major@mhtx.net',
     description="Retrieves CI job URLs from OpenStack Zuul",
